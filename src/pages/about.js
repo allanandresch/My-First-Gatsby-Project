9bf376cf9b6141c8171257fc45a2ba2059a7f7ee
allanandresch <allanandresch@gmail.com>
2020-01-01 21:26:39 -0600

    About.js
    Creating about page

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+    <>
+        <h1>About Me</h1>
+        <p> This is my personal website build in Gatsby super fast</p>
+        <a href="/">&larr; Back To Home</a>
+    </>
+);
